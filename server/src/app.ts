9bc0148b6eff9b73d1d80d8e5a85caee5fd5d799
MHR HABIB <mhrhabib2004@gmail.com>
2025-07-21 02:18:46 +0600

    feat(app): add Zod validation and global error handler integration

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,24 +1,27 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import notFound from './app/middlewares/notFound';
+import { eventRoute } from './app/modules/events/event.route';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
 
 const app: Application = express();
 
-//parsers
+// parsers
 app.use(express.json());
-
-
 app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
 
-// application routes
-// app.use('/', router);
-
+// test route
 app.get('/test', (req: Request, res: Response) => {
-  res.send('Hi Next Level Developer !');
+  res.send('Hi Next Level Developer!');
 });
 
+// application routes - MOUNT BEFORE notFound middleware
+app.use('/', eventRoute);  // Added versioning and better path
 
-//Not Found
+// global error handler
+app.use(globalErrorHandler);
+
+// Not Found - THIS SHOULD BE LAST
 app.use(notFound);
 
 export default app;
