34685199784754e8d0a19289e1384afefb2c2f90
MHR HABIB <mhrhabib2004@gmail.com>
2025-07-22 22:06:34 +0600

    Fixed CORS issue in app.ts and added ThemeProvider in app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,17 +4,25 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ManageEvents from "./components/ManageEvents/ManageEvents";
 import Navbar from "./components/Navbar/Navbar";
 import { Home } from "./pages/Home";
+import Footer from "./shared/Footer/Footer";
+import { ThemeProvider } from "./components/ThemeProvider/theme-provider";
 
 function App() {
   return (
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/manage-events" element={<ManageEvents />} />
       </Routes>
+      <footer className="mt-10">
+        <Footer />
+      </footer>
     </Router>
+</ThemeProvider>
   );
 }
 
 export default App;
+
--- server/src/app.ts
+++ server/src/app.ts
@@ -8,7 +8,14 @@ const app: Application = express();
 
 // parsers
 app.use(express.json());
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://mini-event-scheular-client.vercel.app'  // ✅ spelling ঠিক আছে কিনা দেখো
+  ],
+  credentials: true,
+}));
 
 // test route
 app.get('/test', (req: Request, res: Response) => {
