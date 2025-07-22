decc07c5c73b7bc5667809622b9bbaccea6e245b
MHR HABIB <mhrhabib2004@gmail.com>
2025-07-22 21:58:32 +0600

    Implemented dark/light theme toggle and updated responsive navbar & footer styles

---
+++ client/src/components/ThemeProvider/ModeToggle.tsx
@@ -0,0 +1,22 @@
+import { Sun, Moon } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { useTheme } from "@/components/ThemeProvider/use-theme"
+
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme()
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark")
+  }
+
+  return (
+    <Button variant="outline" size="icon" onClick={toggleTheme}>
+      {theme === "dark" ? (
+        <Moon className="h-[1.2rem] w-[1.2rem]" />
+      ) : (
+        <Sun className="h-[1.2rem] w-[1.2rem]" />
+      )}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  )
+}
---
+++ client/src/components/ThemeProvider/theme-provider.tsx
@@ -0,0 +1,58 @@
+/* eslint-disable react-refresh/only-export-components */
+import { createContext, useEffect, useState } from "react"
+
+type Theme = "light" | "dark" | "system"
+
+type ThemeProviderProps = {
+  children: React.ReactNode
+  defaultTheme?: Theme
+  storageKey?: string
+}
+
+type ThemeProviderState = {
+  theme: Theme
+  setTheme: (theme: Theme) => void
+}
+
+export const ThemeProviderContext = createContext<ThemeProviderState>({
+  theme: "system",
+  setTheme: () => null,
+})
+
+export function ThemeProvider({
+  children,
+  defaultTheme = "system",
+  storageKey = "vite-ui-theme",
+}: ThemeProviderProps) {
+  const [theme, setTheme] = useState<Theme>(
+    () => (localStorage.getItem(storageKey) as Theme) || defaultTheme
+  )
+
+  useEffect(() => {
+    const root = document.documentElement
+    root.classList.remove("light", "dark")
+
+    const appliedTheme =
+      theme === "system"
+        ? window.matchMedia("(prefers-color-scheme: dark)").matches
+          ? "dark"
+          : "light"
+        : theme
+
+    root.classList.add(appliedTheme)
+  }, [theme])
+
+  const value: ThemeProviderState = {
+    theme,
+    setTheme: (theme: Theme) => {
+      localStorage.setItem(storageKey, theme)
+      setTheme(theme)
+    },
+  }
+
+  return (
+    <ThemeProviderContext.Provider value={value}>
+      {children}
+    </ThemeProviderContext.Provider>
+  )
+}
