50e4d14c08f0621f09a611fa94af64558d9f5d33
MHR HABIB <mhrhabib2004@gmail.com>
2025-07-22 06:17:27 +0600

    refactor(types): improve and update TypeScript type definitions

---
+++ client/src/types/event.ts
@@ -0,0 +1,18 @@
+export interface IEvent {
+  _id: string;
+  title: string;
+  date: string;
+  time: string;
+  notes?: string;
+  category: "Work" | "Personal" | "Other";
+  archived: boolean;
+}
+
+export type EventFormData = {
+  title: string;
+  date: string;
+  time: string;
+  category: "Work" | "Personal" | "Other";
+  notes?: string;
+  archived?: boolean
+};
